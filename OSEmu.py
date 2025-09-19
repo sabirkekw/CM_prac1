cfe3f7a143efdd6d254c0deae0dbf0bf3f4ec929
sabirkekw <smagaramov6@gmail.com>
2025-09-19 05:51:43 +0300

    stage1

--- OSEmu.py
+++ OSEmu.py
@@ -2,29 +2,19 @@ import getpass
 import os
 import sys
 import socket
-import logging
-import xml.etree.ElementTree as ET
-from logger import LoggerXML
 
 def repl():
 
     print("Имя скрипта: ", sys.argv[0])
     print("Переданные аргументы: ", *sys.argv[1:])
 
-    VFS = (os.path.basename(sys.argv[1]) + os.path.dirname(__file__)[2:]) if len(sys.argv[1:]) != 0 \
-        else os.path.dirname(__file__) 
-
-    # logging.basicConfig(filename="info.log", level=logging.INFO, format="%(message)s")
-    # with open("info.log", 'r+') as f:
-    #     f.truncate(0)
-
-    logger = LoggerXML("command_log.xml")
+    VFS = ""
 
     while True:
 
         is_command = True
 
-        cmdinput = input('~$'+getpass.getuser()+'.'+socket.gethostname()+ " " + VFS + ": ")
+        cmdinput = input('~$'+getpass.getuser()+'.'+socket.gethostname()+ "" + VFS + ": ")
 
         command = cmdinput.split()[0]
         args = cmdinput.split()[1:]
@@ -45,9 +35,6 @@ def repl():
             print(os.environ.get(command[1:]))
                 
         else:
-            is_command = False
             print("Неизвестная команда.")
-
-        logger.log_command(cmdinput, is_command)
         
 repl()
--- start.cmd
+++
@@ -1,1 +0,0 @@
-python OSEmu.py VFS.ini
